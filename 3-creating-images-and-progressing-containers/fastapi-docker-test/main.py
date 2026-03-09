de87c4d7223dfdab93658f9356bc0fb2f1d8803d
CarlosBusiness-exe <business10carlos@gmail.com>
2026-03-09 14:33:12 -0300

    feat: simple fastapi project to test docker

---
+++ 3-creating-images-and-progressing-containers/fastapi-docker-test/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"msg":"Hello world!"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
