fe5714f0859274fafab77822ae929dab71270bc8
CarlosBusiness-exe <business10carlos@gmail.com>
2026-03-16 22:27:39 -0300

    74-75 classes | Deep understanding of Bind Mount

--- 3-creating-images-and-progressing-containers/fastapi-docker-test/main.py
+++ 3-creating-images-and-progressing-containers/fastapi-docker-test/main.py
@@ -4,6 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    with open("filesMachine/file.txt", "a") as file:
+        file.write("\nHello world!")
+        file.write("\nI edit this file, and still working")
+        file.write("\n#########")
+        file.write("\n@@@@@@@@@@")
     return {"msg":"Hello world!"}
 
 if __name__ == "__main__":
